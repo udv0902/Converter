d2f04c05e05e79bfcef86d0c487880802b623098
user0902 <udv0902@yandex.ru>
2022-09-18 22:07:18 +0500

    Add files two

--- money_convert.py
+++ money_convert.py
@@ -6,10 +6,12 @@ import config
 resp = requests.get("https://finance.rambler.ru/currencies/")
 soup = BeautifulSoup(resp.text, "lxml")
 res_eur = soup.find_all("div", class_="currency-block__marketplace-value")[4].text
+res_eur = float(res_eur)
 
 resp = requests.get("https://finance.rambler.ru/currencies/")
 soup = BeautifulSoup(resp.text, "lxml")
-res_usd = soup.find("div", class_="currency-block__marketplace-value").text
+res_usd = soup.find_all("div", class_="currency-block__marketplace-value")[0].text
+res_usd = float(res_usd)
 
 bot = telebot.TeleBot(config.token)
 
@@ -20,7 +22,7 @@ def start(message):
 	markup.row("EURO", "USD")
 	# markup.row("USD")
 	if message.text == "/start":
-		msg = bot.send_message(message.chat.id, "Выберите валюту", reply_markup=markup)
+		msg = bot.send_message(message.chat.id, "Нажмите на кнопку нужной валюты", reply_markup=markup)
 		bot.register_next_step_handler(msg, currensy)
 
 
@@ -38,7 +40,11 @@ def currensy(message):
 
 def euro(message):
 	try:
-		bot.send_message(message.chat.id, float(message.text) / float(res_eur))
+		markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+		markup.row("EURO", "USD")
+		rub = float(message.text)
+		evro = round((rub / res_eur), 4)
+		bot.send_message(message.chat.id, evro)
 		bot.send_message(message.chat.id, "Введите команду /start")
 	except ValueError:
 		bot.send_message(message.chat.id, "Введите корректные данные")
@@ -46,7 +52,11 @@ def euro(message):
 
 def usd(message):
 	try:
-		bot.send_message(message.chat.id, float(message.text) / float(res_usd))
+		markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+		markup.row("EURO", "USD")
+		rub = float(message.text)
+		dollar = round((rub / res_usd), 4)
+		bot.send_message(message.chat.id, dollar)
 		bot.send_message(message.chat.id, "Введите команду /start")
 	except ValueError:
 		bot.send_message(message.chat.id, "Введите корректные данные")
